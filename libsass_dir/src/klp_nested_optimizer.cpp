75f7d23660949df28d0e5e8b01afd0b3278456ec
ArcRoot <arcroot@cau.ac.kr>
2024-11-06 16:10:20 +0900

    add log

--- libsass_dir/src/klp_nested_optimizer.cpp
+++ libsass_dir/src/klp_nested_optimizer.cpp
@@ -1,10 +1,24 @@
 #include "sass.hpp"
 #include "sass_context.hpp"
 
-namespace Sass{
+#include <iostream>
+#include <fstream>
+#include <stack>
 
+namespace Sass{
+    using namespace std;
     void sass_compiler_optimization(Sass_Compiler *compiler) {
-    
+        Block_Obj root = compiler->root;
+        ofstream ofs("log.txt",ios::out | ios::app);
+        stack<Statement_Obj> s;
+        s.push(root); 
+        if(ofs.is_open()){
+            while(!s.empty()){
+                Statement_Obj obj=s.top();
+                s.pop();
+                
+            }
+        }
     }
 }
 
