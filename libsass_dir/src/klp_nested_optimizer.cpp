7c69b239b4f7aaa1ab074f65beeed5f1a6368a2d
ArcRoot <arcroot@cau.ac.kr>
2024-11-06 15:56:51 +0900

    namespace

--- libsass_dir/src/klp_nested_optimizer.cpp
+++ libsass_dir/src/klp_nested_optimizer.cpp
@@ -1,6 +1,10 @@
 #include "sass.hpp"
 #include "sass_context.hpp"
 
-void sass_compiler_optimization(Sass_Compiler *compiler) {
+namespace Sass{
 
+    void sass_compiler_optimization(Sass_Compiler *compiler) {
+    
+    }
 }
+
