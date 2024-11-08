b6f8c0978842d868ef82a5aef97975751c2c1580
zzeo14 <wodh7223@cau.ac.kr>
2024-11-08 21:29:49 +0900

    update: print statement_type, content

--- libsass_dir/src/klp_nested_optimizer.cpp
+++ libsass_dir/src/klp_nested_optimizer.cpp
@@ -11,38 +11,42 @@ namespace Sass{
     void sass_compiler_optimization(Sass_Compiler *compiler) {
         Block_Obj root = compiler->root;
         ofstream ofs("log.txt",ios::out | ios::app);
-        ///////////////////////////////////////////////////////////////////
-        //              TODO : stack을 이용하여 recursive하게 자식으로 들어가   //
-        //                     블록 관련 내용 추출하기.                       //
-        //              issue: Block 내부의 각 statement의 type을          //
-        //                     어떻게 polymorphic variable로 처리할 것인가    //
-        //////////////////////////////////////////////////////////////////
-        // stack<Statement_Obj> s;
-        // s.push(root); 
-        // if(!ofs.is_open()) return;
-        // while(!s.empty()){
-        //     Statement_Obj obj = s.top();
+        if(!ofs.is_open()) return;
+        stack<StyleRule_Obj> s;
+        for(int i = 0 ; i <root->length() ; i++){
+            Statement_Obj child = root->get(i);
+            if(child->statement_type() == Statement::RULESET){
+                s.push(root->get(i));
+            }
+            ofs << "statement_type: " << child->statement_type() << endl;
+            ofs << "content: " << child->to_string() << endl;
+            ofs << endl;
+        }
+        
+        while(!s.empty()){
+            StyleRule_Obj obj = s.top();
+            Block_Obj block = obj->block();
+            s.pop();
 
-        //     s.pop();
+            // type, length
+            ofs << "statement_type: " << obj->statement_type() << "\n";
+            ofs << "block length: " << block->length() << "\n";
+            ofs << endl;
 
-        //     if(obj->statement_type() == Statement::RULESET){
-        //          //StyleRule* obj = *(s.top().obj());
-        //     }
-            
-        //     type, length
-        //     ofs << obj->statement_type() << "\n";
-        //     ofs << obj->length() << "\n";
+            for(int i = 0 ; i < block->length() ; i++){
+                Statement_Obj child = block->get(i);
+                if(child->statement_type() == Statement::RULESET){
+                    s.push(child);
+                }
+                ofs << "statement_type: " << child->statement_type() << endl;
+                ofs << "content: " << child->to_string() << endl;
+                ofs << endl;
+            }
 
-        //     for(auto iter : obj->elements()){
-        //         ofs << iter->to_string() << "\n";
-        //         s.push(iter);
-        //     }
-
-        //     ofs << "\n";
-        // }
+            ofs << "\n";
+        }
     
 
         ofs.close();
     }
 }
-
