1aaeb14a1035c76f46d2bb718f2dcccb8e79fc3b
zzeo14 <wodh7223@cau.ac.kr>
2024-11-08 10:42:15 +0900

    update

--- libsass_dir/src/klp_nested_optimizer.cpp
+++ libsass_dir/src/klp_nested_optimizer.cpp
@@ -10,15 +10,27 @@ namespace Sass{
     void sass_compiler_optimization(Sass_Compiler *compiler) {
         Block_Obj root = compiler->root;
         ofstream ofs("log.txt",ios::out | ios::app);
-        stack<Statement_Obj> s;
+        stack<Block_Obj> s;
         s.push(root); 
-        if(ofs.is_open()){
-            while(!s.empty()){
-                Statement_Obj obj=s.top();
-                s.pop();
-                
+        if(!ofs.is_open()) return;
+        while(!s.empty()){
+            Block_Obj obj = s.top();
+            s.pop();
+            
+            // type, length
+            ofs << obj.statement_type() << "\n";
+            ofs << obj.length() << "\n";
+
+            for(auto iter : obj->elements()){
+                ofs << iter->to_string() << "\n";
+                s.push(iter);
             }
+
+            ofs << "\n";
         }
+    
+
+        ofs.close();
     }
 }
 
