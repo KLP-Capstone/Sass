2367e4249f99a7d4bfe1ca60b59b176c5f8c6b05
zzeo14 <wodh7223@cau.ac.kr>
2024-11-08 16:39:38 +0900

    update: TODO

--- libsass_dir/src/klp_nested_optimizer.cpp
+++ libsass_dir/src/klp_nested_optimizer.cpp
@@ -11,28 +11,35 @@ namespace Sass{
     void sass_compiler_optimization(Sass_Compiler *compiler) {
         Block_Obj root = compiler->root;
         ofstream ofs("log.txt",ios::out | ios::app);
-        stack<Statement_Obj> s;
-        s.push(root); 
-        if(!ofs.is_open()) return;
-        while(!s.empty()){
-            Statement_Obj obj = s.top();
-            //s.pop();
+        ///////////////////////////////////////////////////////////////////
+        //              TODO : stack을 이용하여 recursive하게 자식으로 들어가   //
+        //                     블록 관련 내용 추출하기.                       //
+        //              issue: Block 내부의 각 statement의 type을          //
+        //                     어떻게 polymorphic variable로 처리할 것인가    //
+        //////////////////////////////////////////////////////////////////
+        // stack<Statement_Obj> s;
+        // s.push(root); 
+        // if(!ofs.is_open()) return;
+        // while(!s.empty()){
+        //     Statement_Obj obj = s.top();
 
-            // if(obj->statement_type() == Statement::RULESET){
-            //     //StyleRule* obj = *(s.top().obj());
-            // }
+        //     s.pop();
+
+        //     if(obj->statement_type() == Statement::RULESET){
+        //          //StyleRule* obj = *(s.top().obj());
+        //     }
             
-            // //type, length
-            // ofs << obj->statement_type() << "\n";
-            // ofs << obj->length() << "\n";
+        //     type, length
+        //     ofs << obj->statement_type() << "\n";
+        //     ofs << obj->length() << "\n";
 
-            // for(auto iter : obj->elements()){
-            //     ofs << iter->to_string() << "\n";
-            //     s.push(iter);
-            // }
+        //     for(auto iter : obj->elements()){
+        //         ofs << iter->to_string() << "\n";
+        //         s.push(iter);
+        //     }
 
-            ofs << "\n";
-        }
+        //     ofs << "\n";
+        // }
     
 
         ofs.close();
