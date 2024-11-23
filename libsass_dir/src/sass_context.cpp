4604493c2ff0e6ba26a6173f73d7d48ae383abad
zzeo14 <wodh7223@cau.ac.kr>
2024-11-23 23:05:22 +0900

    update: 4 whitespaces -> 2 whitespaces for one indentation

--- libsass_dir/src/sass_context.cpp
+++ libsass_dir/src/sass_context.cpp
@@ -346,7 +346,7 @@ extern "C" {
   inline void init_options (struct Sass_Options* options)
   {
     options->precision = 10;
-    options->indent = "    ";
+    options->indent = "  ";
     options->linefeed = LFEED; // LFEED = '\n'
   }
 
